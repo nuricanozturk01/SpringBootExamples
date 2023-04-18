6ad86ffb480e06c2050a245ccc35c6019aa9114f
nuricanozturk01 <18070006034@stu.yasar.edu.tr>
2023-04-18 17:08:05 +0300

    update

--- AnimalHospitalMicroService/VeterinarianGetService/src/main/java/org/csystem/app/service/animalhospital/veterinarian/service/VeterinarianService.java
+++ AnimalHospitalMicroService/VeterinarianGetService/src/main/java/org/csystem/app/service/animalhospital/veterinarian/service/VeterinarianService.java
@@ -1,5 +1,6 @@
 package org.csystem.app.service.animalhospital.veterinarian.service;
 
+import com.karandev.util.data.error.DataUtil;
 import com.metemengen.animalhospital.data.BeanName;
 import com.metemengen.animalhospital.data.dal.VeterinarianServiceHelper;
 import org.csystem.app.service.animalhospital.veterinarian.dto.CountDTO;
@@ -14,6 +15,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 import java.util.stream.StreamSupport;
+
+import static com.karandev.util.data.error.DataUtil.doForDataService;
+import static org.csystem.util.collection.CollectionUtil.toList;
 
 @Service
 public class VeterinarianService
@@ -34,49 +38,46 @@ public class VeterinarianService
 
     public CountDTO countVeterinarians()
     {
-        return m_veterinarianMapper.toCountDTO(m_veterinarianServiceHelper.countVeterinarians());
+        return doForDataService(() -> m_veterinarianMapper.toCountDTO(m_veterinarianServiceHelper.countVeterinarians()),
+                "VeterinarianService.countVeterinarians");
+
     }
 
     public Optional<VeterinarianDTO> findVeterinarianByDiplomaNo(long diplomaNo)
     {
-        return m_veterinarianServiceHelper.findVeterinarianById(diplomaNo).map(m_veterinarianMapper::toVeterinarianDTO);
+        return doForDataService(() -> m_veterinarianServiceHelper.findVeterinarianById(diplomaNo).map(m_veterinarianMapper::toVeterinarianDTO),
+                "VeterinarianService.findVeterinarianByDiplomaNo");
     }
 
     public VeterinariansDTO findVeterinariansByLastName(String lastName)
     {
-        return m_veterinarianMapper.toVeterinariansDTO(CollectionUtil.toList(m_veterinarianServiceHelper.findVeterinariansByLastName(lastName), m_veterinarianMapper::toVeterinarianDTO));
+        return doForDataService(() -> m_veterinarianMapper.toVeterinariansDTO(toList(m_veterinarianServiceHelper.findVeterinariansByLastName(lastName), m_veterinarianMapper::toVeterinarianDTO)),
+                "VeterinarianService.findVeterinariansByLastName");
         //return m_veterinarianMapper.toVeterinariansDTO(StreamSupport.stream(m_veterinarianServiceHelper.findVeterinariansByLastName(lastName).spliterator(), false)
           //      .map(m_veterinarianMapper::toVeterinarianDTO).collect(Collectors.toList()));
     }
     public VeterinariansDTO findVeterinariansByMonthAndYear(int month, int year)
     {
-        return m_veterinarianMapper.toVeterinariansDTO(CollectionUtil.toList(m_veterinarianServiceHelper.findVeterinariansByMonthAndYear(month, year), m_veterinarianMapper::toVeterinarianDTO));
+        return doForDataService(() -> m_veterinarianMapper.toVeterinariansDTO(toList(m_veterinarianServiceHelper.findVeterinariansByMonthAndYear(month, year), m_veterinarianMapper::toVeterinarianDTO)),
+                "VeterinarianService.findVeterinariansByMonthAndYear");
     }
 
     public VeterinariansWithFullNameDTO findVeterinariansByYearBetween(int begin, int end)
     {
-        return m_veterinarianWithFullNameMapper.toVeterinariansWithFullName(CollectionUtil.toList(m_veterinarianServiceHelper.findVeterinariansByYearBetween(begin,end),
-                m_veterinarianWithFullNameMapper::toVeterinarianWithFullName));
+        return doForDataService(() -> m_veterinarianWithFullNameMapper.toVeterinariansWithFullName(toList(m_veterinarianServiceHelper.findVeterinariansByYearBetween(begin,end), m_veterinarianWithFullNameMapper::toVeterinarianWithFullName)),
+                "VeterinarianService.findVeterinariansByYearBetween");
     }
-
-    /*public VeterinarianSaveDTO saveVeterinarian(VeterinarianSaveDTO veterinarianSaveDTO)
-    {
-        m_veterinarianServiceHelper.save(m_veterinarianSaveMapper.toVeterinarianSave(veterinarianSaveDTO));
-
-        return veterinarianSaveDTO;
-    }*/
 
     public VeterinariansDTO findVeterinarianByYear(int year)
     {
-        return m_veterinarianMapper.toVeterinariansDTO(StreamSupport.stream(m_veterinarianServiceHelper.findVeterinariansByYear(year).spliterator(), false)
-                .map(m_veterinarianMapper::toVeterinarianDTO).toList());
+        return doForDataService(() -> m_veterinarianMapper.toVeterinariansDTO(StreamSupport.stream(m_veterinarianServiceHelper.findVeterinariansByYear(year).spliterator(), false).map(m_veterinarianMapper::toVeterinarianDTO).toList()),
+                "VeterinarianService.findVeterinarianByYear");
     }
 
     public VeterinariansDTO findVeterinarianByMonth(int month)
     {
-        return m_veterinarianMapper.toVeterinariansDTO
-                (CollectionUtil.toList(m_veterinarianServiceHelper.findVeterinariansByMonth(month),
-                        m_veterinarianMapper::toVeterinarianDTO));
+        return doForDataService(() -> m_veterinarianMapper.toVeterinariansDTO(toList(m_veterinarianServiceHelper.findVeterinariansByMonth(month), m_veterinarianMapper::toVeterinarianDTO)),
+                "VeterinarianService.findVeterinarianByMonth");
        /* return m_veterinarianMapper.toVeterinariansDTO(StreamSupport.stream(m_veterinarianServiceHelper.findVeterinariansByMonth(month).spliterator(), false)
                 .map(m_veterinarianMapper::toVeterinarianDTO).toList());*/
     }
